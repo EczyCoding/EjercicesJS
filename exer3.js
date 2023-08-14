4bd5c607d25c91a6096fba145f2ff5d6499bad5f
EczyCooder <141979686+EczyCoding@users.noreply.github.com>
2023-08-13 18:17:33 -0600

    Create exer3.js

---
+++ exer3.js
@@ -0,0 +1,11 @@
+var name = "EAGLE"
+var code = "KJ3KE349"
+
+print("Name: "+name)
+print("Code: "+code)
+
+if(name=="EAGLE" && code=="KJ3KE349") {
+    print("\nSe ha accedido, correctamente "+name+".")
+} else {
+    print("\nNo se ha podido, acceder "+name+".")
+}
