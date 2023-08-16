6dcfca26a03c579da5717fab329eaf70444f3932
EczyCooder <141979686+EczyCoding@users.noreply.github.com>
2023-08-16 15:31:47 -0600

    Add files via upload

---
+++ js/exer4.js
@@ -0,0 +1,14 @@
+var n1, n2, total;
+
+prompt("Calculadora de divisiones: ")
+
+n1=prompt("Ingresad el primer numero: ")
+n2=prompt("Ingresad el segundo numero: ")
+
+total = n1/n2
+
+document.writeln("Numero 1: "+n1)
+document.write(" / ")
+document.writeln("Numero 2: "+n2)
+
+document.write("Total: "+total)
