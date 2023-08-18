8a8c57e08959ab1c602b57d65b5defb10e8eec9a
EczyCooder <141979686+EczyCoding@users.noreply.github.com>
2023-08-18 12:44:50 -0600

    Update exer1.js

--- exer1.js
+++ exer1.js
@@ -1,14 +1,15 @@
-var user = "Example"
+// Puede realizar cambios en las variables "user" y "key" para probar las condicionales.
+var user = "Example1"
 var key = "1234"
 
 if (user == "Example1" && key == "1234") {
-print("El usuario y clave han sido digitados correctamente")
+print("El usuario y clave han sido digitados correctamente.")
 
 } else if (user == "Example1" && key != "1234") {
-    print("El usuario "+user+" ha sido ingresado correctamente, pero la clave "+password+" no ha sido correcta.")
+    print("El usuario "+user+" ha sido ingresado correctamente, pero la clave "+key+" no ha sido correcta.")
     } else if (user != "Example1" && key == "1234") {
-        print("This user "+user+" is correct, but the password "+key" not correct.")
+        print("El usuario "+user+" es incorrecto pero la llave "+key+" si es correcta.")
     }
     else {
-    print("This user "+user+" and key "+key+" not specified.")
+    print("El usuario "+user+" y la llave "+key+" no han sido especificados.")
 }
